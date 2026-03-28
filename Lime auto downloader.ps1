b8f0b988e27029b1e87fd7cedf2be3767c7eeebd
FlaxerzNO1 <oguzhnapp0@gmail.com>
2026-03-28 21:20:55 +0300

    Lime auto downloader.ps1

--- Lime auto downloader.ps1
+++ Lime auto downloader.ps1
@@ -1231,10 +1231,10 @@ $dosyalar = @(
         @{ Url = "https://sourceforge.net/projects/processhacker/files/processhacker2/processhacker-2.39-setup.exe/download"; Ad = "ProcessHacker-2.39-setup.exe" }
         @{ Url = "https://github.com/ponei/JournalTrace/releases/download/1.0/JournalTrace.exe"; Ad = "JournalTrace.exe" }
         @{ Url = "https://github.com/ponei/CachedProgramsList/releases/download/1.1/CachedProgramsList.exe"; Ad = "CachedProgramsList.exe" }
-        @{ Url = "https://privazer.com/ShellBagAnalyzerCleaner.exe"; Ad = "ShellBagAnalyzerCleaner.exe" }
+        @{ Url = "https://privazer.com/en/shellbag_analyzer_cleaner.exe"; Ad = "ShellBagAnalyzerCleaner.exe" }
         @{ Url = "https://dl.echo.ac/tool/journal"; Ad = "Journal.exe" }
         @{ Url = "https://www.nirsoft.net/utils/browsinghistoryview.zip"; Ad = "BrowsingHistoryView.zip" }
-        @{ Url = "https://www.nirsoft.net/utils/webbrowserdownloadsview.zip"; Ad = "WebBrowserDownloadsView.zip" }
+        @{ Url = "https://www.nirsoft.net/utils/browserdownloadsview-x64.zip"; Ad = "WebBrowserDownloadsView.zip" }
         @{ Url = "https://www.nirsoft.net/utils/jumplistsview.zip"; Ad = "JumpListsView.zip" }
         @{ Url = "https://www.nirsoft.net/utils/computeractivityview.zip"; Ad = "ComputerActivityView.zip" }
         @{ Url = "https://www.nirsoft.net/utils/usbdrivelog.zip"; Ad = "USBDriveLog.zip" }
